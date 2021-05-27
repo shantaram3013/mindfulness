177080c0b439e13c97d06a7052445971c2ab3b88
Siddharth Singh <simsid66@gmail.com>
2021-05-27 22:33:41 +0530

    Rework styling, add alert dialog
    - reordered styling to ascend in specificity (mostly)
    - make it a lot more responsive in case there are psychopaths writing 9:18 code
    - input items are now aligned to container edges, a lot more aesthetic,
    - added an alert dialog and helper systems
    - beginnings of audio subsystem
    - credits are _kinda_ fixed now?

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 window.onload = init;
 
 function init() {
+    initialiseAlert();
     let btn = document.querySelector("#remind-me");
     btn.onclick = function(e) {
         e.preventDefault();
@@ -18,11 +19,38 @@ function init() {
     }
 }
 
+function initialiseAlert() {
+    document.getElementById('alert-close').onclick = function() {
+        document.getElementById('mask').style.display = 'none';
+    }
+}
+
+function createAlert(title, body, mode, callback) {
+    document.getElementById('mask').style.display = 'flex';
+    if (callback) {
+        document.getElementById('alert-action').display = 'block';
+    }
+
+    document.getElementById('alert-header').innerHTML = title;
+    document.getElementById('body').innerHTML = body;
+
+    if (mode == 'error') {
+        document.getElementById('alert-close').display = 'none';
+    }
+}
+
 class AlertWorkerManager {
     /*
      * @param duration: a length of time in seconds
      */
-    constructor(duration, heartbeat) {
+    constructor(audio, duration, heartbeat) {
+        this.audio = document.querySelector(audio);
+        if (this.audio) {
+            audio.loop = true;
+        } else {
+            throw new Error("Invalid audio selector specified");
+        }
+
         if (!window.Worker) {
             throw new Error("Web Workers don't seem to be supported in your browser!");
         }
